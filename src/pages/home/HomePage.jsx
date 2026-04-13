c7987932e8591dac1267e020c1aec29106aa4344
Aminul islam <aminurrohman98@gmail.com>
2026-04-13 20:48:23 +0100

    Banner details and title has been added

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,10 +1,68 @@
 import React from "react";
+import BannerFriendsDetails from "../../utility/BannerFriendsDetails";
+
+import { FaPlus } from "react-icons/fa6";
 const HomePage = () => {
+  const friendsDetails = [
+    {
+      id: 1,
+      count: 10,
+      title: "Total Friends",
+    },
+    {
+      id: 2,
+      count: 3,
+      title: "On Track",
+    },
+    {
+      id: 3,
+      count: 6,
+      title: "Need Attention",
+    },
+    {
+      id: 4,
+      count: 12,
+      title: "Need Attention",
+    },
+  ];
   return (
-    <section className="w-full">
-      <div className="max-w-7xl w-full mx-auto">
+    <section className="w-full bg-[#F8FAFC] py-20 ">
+      <div className="max-w-6xl w-full mx-auto">
         {/* This is top level of banner */}
-        <div></div>
+        <div className="flex flex-col justify-center items-center gap-y-10 border-b border-b-black/10 pb-12 mb-10">
+          {/* title and descriptions */}
+          <div className="flex flex-col justify-center items-center gap-y-6">
+            <h1 className="font-bold text-5xl">
+              Friends to keep close in your life
+            </h1>
+            <p className="max-w-130 text-center text-sm font-normal text-[#64748B]">
+              Your personal shelf of meaningful connections. Browse, tend, and
+              nurture the relationships that matter most.
+            </p>
+            <button
+              className="py-2 px-4 rounded cursor-pointer
+             bg-[#244D3F] text-white flex 
+             flex-row justify-center items-center gap-1 font-semibold text-base"
+            >
+              <FaPlus />
+              Add a Friend
+            </button>
+          </div>
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-4 md:gap-8 max-w-full w-full">
+            {friendsDetails.map((friend) => (
+              <BannerFriendsDetails
+                key={friend.id}
+                number={friend.count}
+                title={friend.title}
+              ></BannerFriendsDetails>
+            ))}
+          </div>
+        </div>
+        {/* This is bottom level of banner */}
+        <div>
+          <h2 className="font-semibold text-2xl">Your Friends</h2>
+          {}
+        </div>
       </div>
     </section>
   );
---
+++ src/utility/BannerFriendsDetails.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const BannerFriendsDetails = ({ number, title }) => {
+  return (
+    <div className="flex flex-col justify-center items-center gap-y-2 shadow-[0_0_5px_rgba(0,0,0,0.1)] py-8 rounded-2xl">
+      <h1 className="font-semibold text-3xl text-[#244D3F]">{number}</h1>
+      <p className="font-normal text-[18px] text-black/60">{title}</p>
+    </div>
+  );
+};
+
+export default BannerFriendsDetails;
